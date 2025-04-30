8cc5e7a78f9d29d0dd88c726338b99d30e810177
Naveenbandari26 <bandarin29@gmail.com>
2025-04-30 11:15:57 +0530

    Created the React-Native Base Code including GlueStack UI

---
+++ frontend/App.tsx
@@ -0,0 +1,12 @@
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
+import './global.css';
+
+export default function App() {
+  return (
+    <GluestackUIProvider>
+  
+    </GluestackUIProvider>
+  );
+}
