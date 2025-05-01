68c3b782aa9a240854eb5b1717e741b8be2312a4
sandeep <sandeeppatil10705@gmail.com>
2025-05-01 10:28:00 +0530

    Created SignUp in components and provided the base code

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,11 +2,15 @@ import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 import './global.css';
+import SignUp from "./components/SignUp/SignUp";
+import { Platform } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 
 export default function App() {
   return (
-    <GluestackUIProvider>
-  
+    <GluestackUIProvider mode="light">
+      {Platform.OS === 'web' ? <SignUp /> : <SafeAreaView><SignUp/></SafeAreaView>}
     </GluestackUIProvider>
   );
 }
