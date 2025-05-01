ba7127f4b59ee98edd947dc5f67ac96b6c3fdcd5
Kollimarla naga sai satya teja <nagasai.koli@gmail.com>
2025-05-01 11:14:21 +0530

    modified SignIn Component for alternate signin options and added Singup button

--- frontend/components/siginin/SignIn.tsx
+++ frontend/components/siginin/SignIn.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import { Platform, Dimensions } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+import { Divider } from '../ui/divider';
 
 import { Box } from '@/components/ui/box';
 import { Button, ButtonText } from '@/components/ui/button';
@@ -66,9 +70,39 @@ const SignIn = () => {
                   />
                 </Input>
               </FormControl>
+
               <Button className="rounded-xl bg-sky-700">
                 <ButtonText className="text-white">Sign In</ButtonText>
               </Button>
+              <HStack className="my-4 items-center justify-between">
+                <Divider className="flex-1" />
+                <Text className="mx-3 text-sm text-slate-500">Or Continue With</Text>
+                <Divider className="flex-1" />
+              </HStack>
+
+              <HStack className="mb-4 justify-center space-x-6">
+                <Button
+                  variant="outline"
+                  className="w-1/3 rounded-xl border border-slate-300 bg-white p-3"
+                  onPress={() => console.log('Google Sign-In')}>
+                  <FontAwesome name="google" size={24} />
+                </Button>
+                <Button
+                  variant="outline"
+                  className="w-1/3 rounded-xl border border-slate-300 bg-white p-3"
+                  onPress={() => console.log('LinkedIn Sign-In')}>
+                  <AntDesign name="linkedin-square" size={24} />
+                </Button>
+              </HStack>
+              <HStack className="mt-4 items-center justify-center text-lg">
+                <Text className="text-sm text-slate-500">Don't have an account?</Text>
+                <Button
+                  variant="link"
+                  onPress={() => console.log('Navigate to Sign Up')}
+                  className="ml-2">
+                  <Text className="text-sm font-semibold text-sky-700">Sign Up</Text>
+                </Button>
+              </HStack>
             </VStack>
           </Box>
         </HStack>
