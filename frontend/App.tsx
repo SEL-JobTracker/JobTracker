d52dc8466a8be1a28393d6a28feaec453bd676dd
Kollimarla naga sai satya teja <nagasai.koli@gmail.com>
2025-05-01 10:19:48 +0530

    created SignIn Component and Provided the base code

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,8 @@
-import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import '@/global.css';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 
 import './global.css';
 
 export default function App() {
-  return (
-    <GluestackUIProvider>
-  
-    </GluestackUIProvider>
-  );
+  return <GluestackUIProvider />;
 }
